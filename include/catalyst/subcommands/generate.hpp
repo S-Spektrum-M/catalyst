2346ce2cb61b63c8095b85037ab308a60459e826
Siddharth Mohanty <neosiddharth@gmail.com>
2025-09-04 11:55:34 -0700

    set library_path envvar

--- include/catalyst/subcommands/generate.hpp
+++ include/catalyst/subcommands/generate.hpp
@@ -14,4 +14,14 @@ struct parse_t {
 std::expected<YAML::Node, std::string> profile_composition(const std::vector<std::string> &profiles);
 std::pair<CLI::App *, std::unique_ptr<parse_t>> parse(CLI::App &app);
 std::expected<void, std::string> action(const parse_t &);
+void resolve_vcpkg_dependency(const YAML::Node &dep, const std::string &triplet, std::string &ldflags,
+                              std::string &ldlibs);
+std::expected<void, std::string> resolve_local_dependency(const YAML::Node &dep, std::string &cxxflags,
+                                                          std::string &ccflags, std::string &ldflags,
+                                                          std::string &ldlibs);
+void resolve_pkg_config_dependency(const YAML::Node &dep, std::string &cxxflags, std::string &ccflags,
+                                   std::string &ldflags, std::string &ldlibs);
+void resolve_system_dependency(const YAML::Node &dep, std::string &cxxflags, std::string &ccflags, std::string &ldflags,
+                               std::string &ldlibs);
+std::expected<std::string, std::string> lib_path(const YAML::Node &profile);
 } // namespace catalyst::generate
