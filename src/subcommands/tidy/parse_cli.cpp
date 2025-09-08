67f521cd1cd0929344c2928388c649beb5f36549
Siddharth Mohanty <neosiddharth@gmail.com>
2025-09-08 03:36:27 -0700

    feat: tidy subcommand

--- src/subcommands/tidy/parse_cli.cpp
+++ src/subcommands/tidy/parse_cli.cpp
@@ -3,7 +3,7 @@
 namespace catalyst::tidy {
 
 std::pair<CLI::App *, std::unique_ptr<parse_t>> parse(CLI::App &app) {
-    CLI::App *tidy = app.add_subcommand("add", "add a dependency");
+    CLI::App *tidy = app.add_subcommand("tidy", "run linting");
     auto ret = std::make_unique<parse_t>();
     tidy->add_option("-p,--profiles", ret->profiles);
     return {tidy, std::move(ret)};
