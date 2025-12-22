5323b3696d1296499cd6757ea1cbe78b26a8e9b0
Siddharth Mohanty <neosiddharth@gmail.com>
2025-12-22 05:36:07 -0800

    move away from boost-process

--- src/utils/process_exec.cpp
+++ src/utils/process_exec.cpp
@@ -5,7 +5,7 @@
 #include <string>
 
 /// async wrapper around std::system()
-/// TODO: migrate to safe exec functions like in boost.process
+/// TODO: migrate to safe exec functions like in reprco
 namespace catalyst {
 std::expected<std::future<int>, std::string> process_exec(std::string command) {
     return std::async(std::launch::async, [command] -> int { return std::system(command.c_str()); });
